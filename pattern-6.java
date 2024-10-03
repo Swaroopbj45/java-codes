b065fc171fec91b614553d99d7de9fb3c3cafb39
Swaroopbj45 <159910426+Swaroopbj45@users.noreply.github.com>
2024-10-03 19:34:11 +0530

    Add files via upload

---
+++ pattern-6.java
@@ -0,0 +1,21 @@
+public class Main
+{
+	public static void main(String[] args) {
+	    for(int i=1;i<=5;i++){
+	        for(int k=4;k>=i;k--){
+	            System.out.print(" ");
+	        }
+	       
+	      for(int j=1;j<=(2*i-1);j++)
+	      {
+	          System.out.print('*');
+	      }
+	      System.out.println();
+	 
+	    }
+	 
+	
+	}
+}
+
+		
